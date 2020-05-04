6528f6552b3693ade48e2aa88138a5aa0d6f35d3
merellois <55089973+merellois@users.noreply.github.com>
2020-05-04 12:03:04 +0200

    Add files via upload

---
+++ Outcome/index.js
@@ -0,0 +1,150 @@
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play')
+  var soundOff = document.getElementById('pause')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play1')
+  var soundOff = document.getElementById('pause1')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play2')
+  var soundOff = document.getElementById('pause2')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play3')
+  var soundOff = document.getElementById('pause3')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play4')
+  var soundOff = document.getElementById('pause4')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play5')
+  var soundOff = document.getElementById('pause5')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play6')
+  var soundOff = document.getElementById('pause6')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play7')
+  var soundOff = document.getElementById('pause7')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play8')
+  var soundOff = document.getElementById('pause8')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
+
+function toggleAudio() {
+  var audioElement = document.getElementById('player')
+  var soundOn = document.getElementById('play9')
+  var soundOff = document.getElementById('pause9')
+  if (audioElement.paused) {
+    audioElement.play();
+    $(soundOn).show();
+    $(soundOff).hide();
+  } else {
+    audioElement.pause();
+    $(soundOn).hide();
+    $(soundOff).show();
+  }
+}
